5b707526c9b73e710a1a9150450fd1ef5ad68daa
Pizazzle56 <behrmanz@salinehornets.org>
2016-01-04 06:50:13 -0800

    Initial Commit

---
+++ desktop/src/com/icraftgames/codingclicker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.icraftgames.codingclicker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.icraftgames.codingclicker.CodingClicker;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CodingClicker(), config);
+	}
+}
